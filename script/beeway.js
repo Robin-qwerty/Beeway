2bde86fc2c57b31fc9e3f07eb9f64186f3c018d9
Robin-qwerty <robin@humilis.net>
2023-04-17 16:23:38 +0200

    beeway addrow buttons start

--- script/beeway.js
+++ script/beeway.js
@@ -20,15 +20,27 @@ function allowUncheck(e) {
 }
 
 
-function addobservatie() {
-  alert("dfsadf");
-  var inputsDiv = document.getElementById("textareaobservatie");
-  var inputCount = inputsDiv.getElementsByTagName("input").length + 1;
-  var newInput = document.createElement("div");
-  newInput.innerHTML = '<label for="ingredient' + inputCount + '">ingredient ' + inputCount + ':</label> <br> <input type="text" placeholder="Enter ingredient ' + inputCount + '" name="ingredient' + inputCount + '" required> <button type="button" class="deletebutton" onclick="removeInput(this)">-</button>';
-  inputsDiv.appendChild(newInput);
-}
+// function addobservatie() {
+//   alert("dfsadf");
+//   var inputsDiv = document.getElementById("textareaobservatie");
+//   var inputCount = inputsDiv.getElementsByTagName("input").length + 1;
+//   var newInput = document.createElement("div");
+//   newInput.innerHTML = '<label for="ingredient' + inputCount + '">ingredient ' + inputCount + ':</label> <br> <input type="text" placeholder="Enter ingredient ' + inputCount + '" name="ingredient' + inputCount + '" required> <button type="button" class="deletebutton" onclick="removeInput(this)">-</button>';
+//   inputsDiv.appendChild(newInput);
+// }
+//
+// function removeInput(button) {
+//   button.parentNode.remove();
+// }
 
-function removeInput(button) {
-  button.parentNode.remove();
-}
+
+$(document).ready(function(){
+  $("#addplanning").click(function(){
+    event.preventDefault();
+
+    var done = "<div><input type=radio id=done name=done8></div>";
+
+
+    $("#done").html(done);
+  });
+});
